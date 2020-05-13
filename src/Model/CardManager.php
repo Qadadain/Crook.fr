2bbda3293ab911342297484a62dd606d638b638a
thomas <thomas.luminic@outlook.fr>
2020-05-13 11:36:49 +0200

    fix sql request for favorite and popularity

--- src/Model/CardManager.php
+++ src/Model/CardManager.php
@@ -17,15 +17,22 @@ class CardManager extends AbstractManager
     public function getCardPicture(): array
     {
         $sql = 'SELECT s.id, s.title, s.description, s.content, 
-	                   l.name, l.color, l.image, s.created_at, f.user_id,
-	                   (SELECT SUM(p.vote) FROM popularity p WHERE p.sheet_id = s.id GROUP BY p.sheet_id) popularity
-                FROM sheet s
-                JOIN language l
-                ON s.language_id = l.id
-                LEFT JOIN favorite f
-                ON s.id = f.sheet_id
-                ORDER BY s.created_at 
-                DESC LIMIT 6';
+                    l.name, l.color, l.image, s.created_at';
+        if (isset($_SESSION['id'])) {
+            $sql .= ', (SELECT f.user_id 
+                      FROM favorite f 
+                      WHERE f.sheet_id = s.id 
+                      AND f.user_id = ' . $_SESSION['id'] .') favoris,
+                      (SELECT p.vote
+                      FROM popularity p 
+                      WHERE p.sheet_id = s.id 
+                      AND p.user_id = '. $_SESSION['id'] . ') vote';
+        }
+        $sql .= ' FROM sheet s
+                 JOIN language l
+                 ON s.language_id = l.id
+                 ORDER BY s.created_at
+                 DESC LIMIT 6';
         $statement = $this->pdo->query($sql);
         return $statement->fetchAll();
     }
